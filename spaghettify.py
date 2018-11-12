4cfd53871880f6b34ee24ce2d948371d7fbb4786
RafalKornel <rafal.kornel123@gmail.com>
2018-11-12 01:55:53 +0100

    Cleaned code, used more regular expresions

--- spaghettify.py
+++ spaghettify.py
@@ -7,17 +7,9 @@ result = ""
 with open('input.txt') as _input:
     for line in _input:
         line = re.sub('//.*', ' ', line)
-        text.append(line.replace('\n', ' ').expandtabs(1))
+        text.append(line)
 
 with open('output.txt', 'w+') as _output:
     for line in text:
-        temp_line = ""
-        for i, char in enumerate(line):
-            if i == 0 or i == len(line) - 1:
-                if char != ' ': temp_line += char
-            else:
-                if char == ' ' and ( line[i - 1] == ' ' or line[i + 1] == ' ' ):
-                    pass
-                else:
-                    temp_line += char
-        _output.write(temp_line)
+        line = re.sub('\s+', ' ', line).strip()
+        _output.write(line)
