efc83b366865ea45bfbaa350d95d04178ec84a5e
RafalKornel <rafal.kornel123@gmail.com>
2018-11-15 20:48:36 +0100

    Added dynamic input (you now input file path after launching script), few comments and example folder.

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Take your pretty java code and make it ugly
 
 Usage:
-Insert script in folder with file input.txt, in which put your code. Then use script (python3 spaghettify.py in bash or whatever).
-Script will make file output.txt in the same directory.
+Use script (python3 spaghettify.py in bash or whatever), then input file path (it can be relative or absolut path, including ~/file).
+Note that this script doesn't work on python2.
 
-This script doesn't support comments that aren't basic two backslash (//), so if there is some fancy comment in code
-output will be incorrect.
+Script will make file output.txt in the directory it is located.
--- spaghettify.py
+++ spaghettify.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python
 import re
+from pathlib import Path
 
 text = []
-result = ""
+home = str(Path.home())
 
-with open('input.txt') as _input:
+# Input validation
+while(True):
+    _path = input("Please insert path to the input file: ")
+    if _path[0] == '~': _path = home + _path[1::]
+    try:
+        with open(_path) as test_input:
+            pass
+    except FileNotFoundError:
+        print('This file doesn\' exists!')
+    else:
+        break
+
+# Opening file and removing // comments from each line
+with open(_path) as _input:
+    #print(_input.read())
     for line in _input:
         line = re.sub('//.*', ' ', line)
         text.append(line)
 
+# Creating output file, also removing white characters from each line
 with open('output.txt', 'w+') as _output:
     for line in text:
         line = re.sub('\s+', ' ', line).strip()
